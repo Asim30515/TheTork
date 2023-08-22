c378e7a3316d1134aab0303ea175bd3d8dbc3d6f
Asim <asimhasanli1575@gmail.com>
2023-08-22 17:57:38 +0400

    Yeni layihe yaradildi ve ilk defe elave edildi

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['thetork.html'],
+  theme: {
+    extend: {
+      fontSize:{
+        "font":"9px",'fot':'27px','fo':'22px','fr':'8px','ft':'18px','px':'34px','gt':'11px',
+      },
+      backgroundColor:{
+        'reng':'#1a1f5d','renq':'#03062f','renc':'#010317','re':'#5f51fd', 'tt':'#2f287e','rent':'#04113c', 'gner':'#151a3e'
+      },
+      color:{
+        'freng':'#1BADFC',
+      },
+      width:{
+        'tr':'32%',
+      },
+      backgroundImage:{
+        'img1':'url(./bg1.jpg)'
+      }
+    },
+  },
+  plugins: [],
+}
+
